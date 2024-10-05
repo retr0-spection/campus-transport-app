726ff27cbf69beeb9e17a9a54daf3d7a50cb783b
Teddy <mteddy941@gmail.com>
2024-10-05 16:47:40 +0200

    save

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -255,7 +255,7 @@ export default function App() {
     fetchData();
     getCurrentLocation();
     _getRentalStations();
-    registerForPushNotifications();
+    //registerForPushNotifications();
   }, []);
 
   React.useEffect(() => {
